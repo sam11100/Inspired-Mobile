6af2004b75bd75b086232c47e6535fc35a28c3a8
Samir Thapa <sam11100@hotmail.co.uk>
2016-12-22 11:53:41 +0000

    Inspired Mobile

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(window).ready(function(){
+
+
+	$('#myModal').modal('show');
+
+
+});	
+
+window.onload = function(){
+
+
+	var clickUrl = document.getElementById('click2');
+
+
+	clickUrl.addEventListener('click',function(){
+
+
+		window.open('');
+
+	});
+
+}
